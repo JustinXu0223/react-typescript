d04cb34c5ebc2359405c1944610b4190e94b9b55
JOKER XU <18696790771@163.com>
2018-01-08 11:24:03 +0800

    去除reject 增加antd完成

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,10 @@
 /* config-overrides.js */
-const tsImportPluginFactory = require('ts-import-plugin');
-const { getLoader } = require("react-app-rewired");
+// const tsImportPluginFactory = require('ts-import-plugin');
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-    const tsLoader = getLoader(
+   /* const tsLoader = getLoader(
         config.module.rules,
         rule =>
             rule.loader &&
@@ -19,7 +20,10 @@ module.exports = function override(config, env) {
                 style: 'css',
             }) ]
         })
-    };
-
+    };*/
+    config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+    config = rewireLess.withLoaderOptions({
+        modifyVars: { "@primary-color": "#1DA57A" },
+    })(config, env);
     return config;
 }
